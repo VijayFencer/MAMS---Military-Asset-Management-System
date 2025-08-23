b08132a756de86761c538cdd2e813369d64265dc
VijayFencer <vijaybharathslmfencing@gmail.com>
2025-08-23 11:10:45 +0530

    deploy

--- mams-frontend/src/services/api.tsx
+++ mams-frontend/src/services/api.tsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://localhost:5000/api" });
+const api = axios.create({ 
+  baseURL: "https://mams-military-asset-management-system.onrender.com/api" 
+});
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
